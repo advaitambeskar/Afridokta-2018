56fb7700bc585dc535a16f2cba71ea93e588102e
Justin P <justintime5046@gmail.com>
2017-10-29 14:13:32 -0400

    Started Welcome Primary Selection

--- Assets/Welcome.cs
+++ Assets/Welcome.cs
@@ -8,32 +8,43 @@ public class Welcome : MonoBehaviour {
 	public int secondsToWait;
 	public RawImage logo;
 	public Text text;
+	public CanvasGroup primarySelection;
 
-	private bool show;
+	private bool showLogo;
 
 	// Use this for initialization
 	void Start () {
 		logo = logo.GetComponent<RawImage>();
 		text = text.GetComponent<Text>();
+		primarySelection = primarySelection.GetComponent<CanvasGroup>();
 
-		show = true;
-
+		showLogo = true;
+		primarySelection.alpha = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!show) {
+		if (!showLogo) {
 			logo.CrossFadeAlpha(0.0f, 1.0f, false);
 			text.CrossFadeAlpha(0.0f, 1.0f, false);
+			StartCoroutine (FadeInSelection ());
 		} else {
 			StartCoroutine(ShowCoroutine());
 		}
 	}
 
 	IEnumerator ShowCoroutine() {
-		Debug.Log("Before waiting " + secondsToWait + " seconds");
+		//Debug.Log("Before waiting " + secondsToWait + " seconds");
 		yield return new WaitForSeconds(secondsToWait);
-		Debug.Log("After Waiting " + secondsToWait + " seconds");
-		show = false;
+		//Debug.Log("After Waiting " + secondsToWait + " seconds");
+		showLogo = false;
+	}
+
+	IEnumerator FadeInSelection (){
+		while (primarySelection.alpha < 1) {
+			//Debug.Log ("Fading In primary Selection");
+			primarySelection.alpha += Time.deltaTime / 25; //Fades-in over 1 second
+			yield return null;
+		}
 	}
 }
