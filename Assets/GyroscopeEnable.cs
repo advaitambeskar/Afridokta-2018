8bd3473bcb17856f2bb88546d53937defa733427
Justin P <justintime5046@gmail.com>
2017-11-01 17:56:52 -0400

    Welcome + GoogleCardboard + Headmovement Track

---
+++ Assets/GyroscopeEnable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroscopeEnable : MonoBehaviour {
+
+	private Gyroscope gyro;
+
+	// Use this for initialization
+	void Start () {
+		if (SystemInfo.supportsGyroscope) {
+			gyro = Input.gyro;
+			gyro.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
