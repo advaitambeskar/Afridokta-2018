271a5ba4e2f2a00d159e05f2560f9fbb3b59e140
parth <parthsethi16.ps@gmail.com>
2017-11-27 18:07:23 -0500

    parth in class

---
+++ Assets/gotomaleselect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gotomaleselect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void gotomale()
+    {
+        SceneManager.LoadScene("MaleSelect");
+
+    }
+}
---
+++ Assets/scripts/Part.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Part : MonoBehaviour {
+    public Text mytext;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void onClick()
+    {
+        mytext.text = this.ToString();
+    }
+}
