89d7a80966ffe850b6049163cb6bee7fecd0a7a0
Natehooper <natehoop@gmail.com>
2017-12-03 16:50:58 -0500

    female-fixes

---
+++ Assets/gotofemaleselect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gotofemaleselect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+    public void gotofemale()
+    {
+        SceneManager.LoadScene("FemaleSelect");
+
+    }
+}
