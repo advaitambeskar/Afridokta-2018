3b056e7ad9802436a7bd100595c80d0a723ca8a4
JasonHo <hexing_h@hotmail.com>
2018-03-26 02:42:05 -0400

    Modified part script to add highlight function

--- Assets/scripts/Part.cs
+++ Assets/scripts/Part.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Part : MonoBehaviour {
     public Text mytext;
+	public Light myLight;
 	// Use this for initialization
 	void Start () {
 		
@@ -17,7 +18,19 @@ public class Part : MonoBehaviour {
     public void onClick()
     {
         mytext.text = this.ToString();
-        mytext.text = mytext.text.Substring(0, mytext.text.Length - 6);
+        mytext.text = mytext.text.Substring(0, mytext.text.Length - 7);
+		switch (mytext.text) {
+			case "UpperHead":
+				myLight = GameObject.Find("Point light head").GetComponent<Light>();
+				myLight.enabled = !myLight.enabled;
+				break;
+			case "LeftChest":
+				myLight = GameObject.Find("Point light chest").GetComponent<Light>();
+				myLight.enabled = !myLight.enabled;
+				break;
+			default:
+				break;
+		}
 
     }
 }
