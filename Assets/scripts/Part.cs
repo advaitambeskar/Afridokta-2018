46822260ba2da6297cec848f630dfa641b7272ec
Shreya <shreyanirmalan@gmail.com>
2018-03-29 18:06:09 -0400

    added more buttons for symptoms

--- Assets/scripts/Part.cs
+++ Assets/scripts/Part.cs
@@ -6,9 +6,13 @@ using UnityEngine.UI;
 public class Part : MonoBehaviour {
     public Text mytext;
 	public GameObject button;
+	public GameObject button2;
+	public GameObject button3;
 	// Use this for initialization
 	void Start () {
 		button.SetActive (false);
+		button2.SetActive (false);
+		button3.SetActive (false);
 	}
 	
 	// Update is called once per frame
@@ -20,6 +24,8 @@ public class Part : MonoBehaviour {
         mytext.text = this.ToString();
         mytext.text = mytext.text.Substring(0, mytext.text.Length - 6);
 		button.SetActive (true);
+		button2.SetActive (true);
+		button3.SetActive (true);
 
     }
 }
